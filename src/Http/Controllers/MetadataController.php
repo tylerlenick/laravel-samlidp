4cc5a94b3a71adf93c1842d99f1946a7ca906be9
Tyler Lenick <>
2022-01-28 10:32:09 -0500

    comment out debugbar off

--- src/Http/Controllers/MetadataController.php
+++ src/Http/Controllers/MetadataController.php
@@ -15,14 +15,14 @@ class MetadataController extends Controller
     public function index()
     {
         // Check for debugbar and disable for this view
-        if (class_exists('\Barryvdh\Debugbar\Facade')) {
+        /*if (class_exists('\Barryvdh\Debugbar\Facade')) {
             \Barryvdh\Debugbar\Facade::disable();
-        }
+        }*/
 
         $cert = Storage::disk('samlidp')->get(config('samlidp.certname', 'cert.pem'));
         $cert = preg_replace('/^\W+\w+\s+\w+\W+\s(.*)\s+\W+.*$/s', '$1', $cert);
         $cert = str_replace(PHP_EOL, "", $cert);
-
+        
         return response(view('samlidp::metadata', compact('cert')), 200, [
             'Content-Type' => 'application/xml',
         ]);
